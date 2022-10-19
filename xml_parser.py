da84f203bb2034bd7928fda2b077094fc8117c45
Vojtěch Giesl <vojtech.giesl@citiq.cz>
2022-10-19 22:02:37 +0200

    Integrate data insertion into mongo db

--- xml_parser.py
+++ xml_parser.py
@@ -5,7 +5,21 @@ from pathlib import Path
 import xmltodict
 from xml.parsers.expat import ExpatError
 import json
-import zipfile
+from mongo import *
+import traceback
+
+collection = None
+
+
+def setup_db():
+    global collection
+    # Get the database
+    get_databases()
+    # `app` is name of db, `trains` is name of collection
+    create_collection("app", "trains")
+    # get_database `app` our database we dont need anything else imo
+    dbname = get_database()
+    collection = dbname["trains"]
 
 
 def create_arg_parser():
@@ -18,24 +32,29 @@ def create_arg_parser():
 
 
 def process_json(obj: json):
-    ...
+    # Showcase how to use insert https://www.w3schools.com/python/python_mongodb_insert.asp
+    # collection.insert_many([item_1,item_2])
+    collection.insert_many([obj])
 
-def parse_xml_dir(path: Path):
+
+def parse_xml_dir(path: str = "./xmls"):
     xml_errors = []
     json_errors = []
-    for filename in os.listdir(path):
-        print(filename)
-        with open(path + "/" + filename, mode="r", encoding="utf-8") as xml_file:
-            try:
-                data_dict = xmltodict.parse(xml_file.read())
-
+    print(f"parsing xml files")
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            # print(f"parsing: {file}")
+            with open(os.path.join(root, file), "rb") as xml_file:
                 try:
-                    jo = json.dumps(data_dict)
-                    process_json(jo)
-                except TypeError:
-                    json_errors.append(filename)
-            except ExpatError:
-                xml_errors.append(filename)
+                    data_dict = xmltodict.parse(xml_file.read())
+                    try:
+                        collection.insert_one(data_dict)
+
+                    except TypeError as te:
+                        traceback.print_exc()
+                        json_errors.append(file)
+                except ExpatError as ee:
+                    xml_errors.append(file)
 
     print(f"XML errors: {len(xml_errors)}")
     print(f"JSON errors: {len(json_errors)}")
@@ -48,5 +67,5 @@ if __name__ == "__main__":
     if not os.path.exists(parsed_args.inputDirectory):
         print("Directory not exists")
         sys.exit(1)
+    setup_db()
     parse_xml_dir(parsed_args.inputDirectory)
-

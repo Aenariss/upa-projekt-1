6a3fca809f786560f6a810c87f21d5d04fb5a795
aenariss <aenariss@gmail.com>
2022-10-22 21:35:46 +0200

    stuff

---
+++ client.py
@@ -0,0 +1,66 @@
+from mongo import *
+from datetime import timedelta, datetime
+
+collection_trains = None
+collection_canceled = None
+collection_changes = None
+collection_stations = None
+
+def setup_db():
+    global collection_trains
+    global collection_canceled
+    global collection_changes
+    global collection_stations
+
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client['test']
+    collection_trains = db["trains"]
+    collection_canceled = db["canceled"]
+    collection_changes = db["changes"]
+    collection_stations = db["stations"]
+
+def trainCanceled(id, date):
+    # get = collection_canceled.find_one({"_id":id}) # the same as below, keep just in case
+    get = collection_canceled.find_one(id)  # if it wasnt found, we get None type
+    if get: # if a train was cancelled, check if the day was also
+        # start date
+        start_date = get['CZCanceledPTTMessage']['PlannedCalendar']['ValidityPeriod']['StartDateTime']
+        end_date = get['CZCanceledPTTMessage']['PlannedCalendar']['ValidityPeriod']['EndDateTime']
+        if start_date and end_date:
+            d1 = datetime.fromisoformat(start_date)
+            d2 = datetime.fromisoformat(end_date) + timedelta(days=1) - timedelta(seconds=1)  # end date is the day when it ends, aka the last day of the cancelation, so add + 1 dat to this and subtract 1 second
+            date = datetime.fromisoformat(date)
+
+            # time makes sense, jut a check (they would NOT cancel it so that the cancellation ends before it begins... surely not...)
+            if (d1 <= date <= d2):
+                index = (date-d1).days
+                try:
+                    bitDay = get['CZCanceledPTTMessage']['PlannedCalendar']['BitmapDays'][index]
+                    return bool(bitDay)  # returns 1 if canceled, 0 if not
+                # If the bit day is not set or something is broken in some other way, lets suppose the train runs
+                except:
+                    return False
+            else:
+                return False
+
+def find_common(odkud, kam):
+    # spolecne vlaky pro odkud a kam
+    # https://stackoverflow.com/a/42302818/13279982
+    tmp = [
+            { "$match": { "_id": { "$in": [ "Studénka", "Ostrava-Svinov" ] } } },   # find matching records
+            { "$group": { "_id": 0, "first": { "$first": "$pa" }, "second": { "$last": "$pa" } } },     # remove duplicates
+            { "$project": { "cores": { "$setIntersection": [ "$first", "$second" ] }, "_id": 0 } }      # create a result
+        ]
+    result = collection_stations.aggregate(tmp)
+    try:
+        result = list(result)[0]['cores']
+    except:
+        result = []
+    return result
+
+if __name__ == '__main__':
+    setup_db()
+    print(trainCanceled('KT------694A', '2021-12-12T00:00:00'))
+
+# zruseni vlaku -- DONE
+# nahradni trasa -- IN PROGRESS
--- xml_parser.py
+++ xml_parser.py
@@ -1,6 +1,7 @@
 import os.path
 import sys
 import argparse
+from tracemalloc import start
 import xmltodict
 from xml.parsers.expat import ExpatError
 from mongo import *
@@ -9,24 +10,32 @@ import traceback
 collection_trains = None
 collection_canceled = None
 collection_changes = None
+collection_stations = None
 
 
 def setup_db():
     global collection_trains
     global collection_canceled
     global collection_changes
+    global collection_stations
+
 
     # Get the database
-    get_databases()
-    # `app` is name of db, `trains` is name of collection
-    create_collection("app", "trains")
-    create_collection("app", "canceled")
-    create_collection("app", "changes")
-    # get_database `app` our database we dont need anything else imo
-    dbname = get_database()
-    collection_trains = dbname["trains"]
-    collection_canceled = dbname["canceled"]
-    collection_changes = dbname["changes"]
+    #get_databases()
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client['test']
+
+    collection_trains = db["trains"]
+    collection_trains.drop()
+    collection_canceled = db["canceled"]
+    collection_canceled.drop()
+    collection_changes = db["changes"]
+    collection_changes.drop()
+
+    collection_stations = db["stations"]
+    collection_stations.drop()
+
+    # u changed ziskavat ID pres core RelatedPlannedTransportIdentifiers, tim ziskam odkaz rovnou na origo a z origa muzu hned jet tu k tomu... snad jsou unique
 
 
 def create_arg_parser():
@@ -66,13 +75,81 @@ def parse_xml_dir(path: str = "./xmls"):
     print(f"XML errors: {len(xml_errors)}")
     print(f"JSON errors: {len(json_errors)}")
 
+def getID(xml):
+    id = None
+    try:
+        id = xml['CZPTTCISMessage']['Identifiers']['PlannedTransportIdentifiers'][0]['Core']
+    except:
+        pass
+    return id
+
+def canceledMessageId(orig_msg):
+    id = None
+    # always take the first PlannedTransportIdentifiers, aka PT Core
+    id = orig_msg['CZCanceledPTTMessage']['PlannedTransportIdentifiers'][0]['Core']   
+    id_dict = {"_id":id}
+    orig_msg = {**id_dict, **orig_msg}
+    return orig_msg
+
+def trainStopsInStation(location):
+    try:
+        activityType = location['TrainActivity']
+        try:
+            x = '0001' in activityType['TrainActivityType']
+            if not x: 
+                return 0
+        except:
+            flag = 0
+            for i in activityType:
+                if '0001' in i['TrainActivityType']:
+                    flag = 1
+            if not flag:
+                return 0
+    except:
+        return 0    # train doesnt stop here
+    return 1
+
+def location_collection(data_dict):
+    id = getID(data_dict) # always save core PA
+    stations = data_dict['CZPTTCISMessage']['CZPTTInformation']['CZPTTLocation']
+    for location in stations:
+        
+        # if the activity is not 0001, dont care
+        if trainStopsInStation(location):
+            location = location['Location']['PrimaryLocationName']
+
+            locations = collection_stations.find_one({'_id':location})
+            if not locations:
+                locations = []
+            else:
+                locations = locations['pa']
+            locations.append(id)
+            collection_stations.replace_one({'_id':location}, {"_id":location, 'pa':locations}, upsert=True)
+
+def tmp_push():
+    xml_file = open('./xmls/PA_0054_KT------694A_00_2022.xml', 'rb')
+    xml_file_cancel = open('./xmls/2021-12/cancel_PA_0054_KT------694A_00_2022_20211212.xml.xml', 'rb')
+    xml_file_cancel_2 = open('./xmls/2021-12/cancel_PA_0054_KT----38548A_00_2022_20211212.xml.xml', 'rb')
+    data_dict = xmltodict.parse(xml_file.read())
+    data_dict_cancel = xmltodict.parse(xml_file_cancel.read())
+    data_dict_cancel_2 = xmltodict.parse(xml_file_cancel_2.read())
+
+    location_collection(data_dict)
+
+    data_dict_cancel = canceledMessageId(data_dict_cancel)
+    data_dict_cancel_2 = canceledMessageId(data_dict_cancel_2)
+
+    collection_canceled.insert_one(data_dict_cancel)
+    collection_canceled.insert_one(data_dict_cancel_2)
+    collection_trains.insert_one(data_dict)
 
 if __name__ == "__main__":
 
     arg_parser = create_arg_parser()
-    parsed_args = arg_parser.parse_args(sys.argv[1:])
-    if not os.path.exists(parsed_args.inputDirectory):
-        print("Directory not exists")
-        sys.exit(1)
+    #parsed_args = arg_parser.parse_args(sys.argv[1:])
+    #if not os.path.exists(parsed_args.inputDirectory):
+    #    print("Directory not exists")
+    #    sys.exit(1)
     setup_db()
-    parse_xml_dir(parsed_args.inputDirectory)
+    #parse_xml_dir(parsed_args.inputDirectory)
+    tmp_push()

0253f9e8db3e297dae86753e84245172436eaa27
Vojtěch Giesl <vojtech.giesl@citiq.cz>
2022-10-23 02:41:09 +0200

    Added route finding for normal schedule

--- client.py
+++ client.py
@@ -49,7 +49,7 @@ def find_common(odkud, kam):
     # spolecne vlaky pro odkud a kam
     # https://stackoverflow.com/a/42302818/13279982
     tmp = [
-            { "$match": { "_id": { "$in": [ "Studénka", "Ostrava-Svinov" ] } } },   # find matching records
+            { "$match": { "_id": { "$in": [ odkud, kam ] } } },   # find matching records
             { "$group": { "_id": 0, "first": { "$first": "$pa" }, "second": { "$last": "$pa" } } },     # remove duplicates
             { "$project": { "cores": { "$setIntersection": [ "$first", "$second" ] }, "_id": 0 } }      # create a result
         ]
@@ -60,9 +60,111 @@ def find_common(odkud, kam):
         result = []
     return result
 
+def exist_changed_plan(train, from_station, to_station, dt)-> dict:
+    ...
+
+def get_route(trains:list, from_station, to_station, dt:datetime):
+    for train in trains:
+        if trainCanceled(train, dt.isoformat()):
+            if exist_changed_plan(train, from_station, to_station, dt):
+                ...
+            trains.remove(train)
+
+    hour = dt.hour
+    minute = dt.minute
+    time_int = int(hour)*100 + minute
+    aggregate_query = [{
+        '$match': {
+                    'CZPTTCISMessage.Identifiers.PlannedTransportIdentifiers.Core': {"$in": trains}
+                }
+    },
+        {
+            '$match': {
+            '$and': [
+                {
+                    'CZPTTCISMessage.CZPTTInformation.CZPTTLocation.Location.PrimaryLocationName': f'{from_station}'
+                },
+                {
+                    'CZPTTCISMessage.CZPTTInformation.CZPTTLocation.TimingAtLocation.Timing.time_int': {
+                        '$gte': time_int
+                    }
+                }
+            ]
+        }
+    }
+    ]
+
+
+    query_result = collection_trains.aggregate(aggregate_query)
+    min_value = 3000
+    min_result = None
+    for result in query_result:
+        CZPTTLocation = result["CZPTTCISMessage"]["CZPTTInformation"]["CZPTTLocation"]
+        calendar = result["CZPTTCISMessage"]["CZPTTInformation"]["PlannedCalendar"]
+        day_bitmap = calendar["BitmapDays"]
+        date_start = calendar["ValidityPeriod"]["StartDateTime"] + "-00:00"
+        d1 = datetime.fromisoformat(date_start)
+        index = (dt - d1).days
+        try:
+            bitDay = day_bitmap[index]
+            if bitDay == "0":  # returns 1 if canceled, 0 if not
+                break
+        # If the bit day is not set or something is broken in some other way, lets suppose the train runs
+        except:
+            break
+
+        for location in CZPTTLocation:
+            station = location["Location"]["PrimaryLocationName"]
+            if station == from_station:
+                timings = location["TimingAtLocation"]["Timing"]
+                if type(timings) is not list:
+                    timings = [timings]
+                for timing in timings:
+                    if timing["@TimingQualifierCode"] == "ALD":
+                        if min_value > timing["time_int"] >= time_int:
+                            min_value = timing["time_int"]
+                            min_result = result["CZPTTCISMessage"]
+
+            if station == to_station:
+                break
+    return min_result
+def print_route(CZPTTCISMessage:dict, from_station, to_station):
+    if CZPTTCISMessage is None:
+        print("No route in selected day")
+        return
+    CZPTTLocation = CZPTTCISMessage["CZPTTInformation"]["CZPTTLocation"]
+    print_out = False
+    for location in CZPTTLocation:
+        station = location["Location"]["PrimaryLocationName"]
+        if station == from_station:
+            print_out = True
+        if print_out:
+
+            if trainStopsInStation(location):
+                timings = location["TimingAtLocation"]["Timing"]
+                if type(timings) is not list:
+                    timings = [timings]
+                for timing in timings:
+                    if timing["@TimingQualifierCode"] == "ALD":
+                        print(f'{timing["Time"][:8]} (GMT{timing["Time"][-6:]}) - {station}')
+        if station == to_station:
+            break
+
+
 if __name__ == '__main__':
     setup_db()
-    print(trainCanceled('KT------694A', '2021-12-12T00:00:00'))
+    s_from = "Slaný předměstí"
+    s_to = "Chlumčany u Loun"
+    for i in range(10, 30):
+        date_str = f"2022-05-{i}T00:00:00.000-00:00"
+
+        #trains = find_common(s_from, s_to)
+        trains = ["KT----10208A"]
+        dt = datetime.fromisoformat(date_str)
+        route = get_route(trains, s_from, s_to, dt)
+        print_route(route, s_from, s_to)
+        print("------------------")
+
 
 # zruseni vlaku -- DONE
 # nahradni trasa -- IN PROGRESS

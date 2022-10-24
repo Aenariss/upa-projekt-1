99c297cfd9dc2fcbece5da27341f1c8fc35b8e36
Vojtěch Giesl <vojtech.giesl@citiq.cz>
2022-10-24 20:21:51 +0200

    change order of parsing files

--- xml_parser.py
+++ xml_parser.py
@@ -45,77 +45,119 @@ def parse_xml_dir(collection_trains, collection_stations, path: str = "./xmls"):
     xml_errors = []
     json_errors = []
     root_files = 0
-    non_root_files = 0
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            with open(os.path.join(root, file), "rb") as xml_file:
+    canceled_num = 0
+    changes_num = 0
+
+    dirs = []
+    print(f"parsing {path}")
+    for filename in os.listdir(path):
+        # first iterate files in root dir
+        d = os.path.join(path, filename)
+        if os.path.isdir(d):
+            dirs.append(d)
+        else:
+            #regular file
+            with open(os.path.join(path, filename), "rb") as xml_file:
                 try:
                     data_dict = xmltodict.parse(xml_file.read())
-                    try:
-                        root_flag = (root[:7] != "./xmls/")
-
-                        if root_flag:
-                            root_files += 1
-                            id = getID(data_dict)  # always save core PA
-                            data_dict['_id'] = id
-                            get_location_time(data_dict["CZPTTCISMessage"]["CZPTTInformation"]["CZPTTLocation"])
-                            location_collection(data_dict, collection_stations)
-                            collection_trains.replace_one({'_id':id}, data_dict, upsert=True)
-
-                        else:
-                            non_root_files += 1
-                            if "cancel_" in xml_file.name:
-                                canceledMessageParse(data_dict, collection_trains)
-                                print('ok')
-                            else:   # replacement trains
-                                id = getID(data_dict)
-                                data_dict["_id"] = id
-                                orig_train_id = getIDReplaced(data_dict)    # if None, there is no original train
-                                if orig_train_id:   # original trains which this one replaces was found
-                                    orig_train = collection_trains.find_one({"_id":orig_train_id}) # get the original train's info
-                                    if not orig_train:    # cancelation message doesnt cancel anything
-                                        pass # if original was not found, just put this new one into the collection
-                                    else:
-                                        start_date = data_dict['CZPTTCISMessage']['CZPTTInformation']['PlannedCalendar']['ValidityPeriod']['StartDateTime']
-                                        end_date = data_dict['CZPTTCISMessage']['CZPTTInformation']['PlannedCalendar']['ValidityPeriod']['EndDateTime']
-                                        newBitField = data_dict['CZPTTCISMessage']['CZPTTInformation']['PlannedCalendar']['BitmapDays']
-
-                                        orig_start = orig_train['CZPTTCISMessage']['CZPTTInformation']['PlannedCalendar']['ValidityPeriod']['StartDateTime']
-                                        bitDayField = orig_train['CZPTTCISMessage']['CZPTTInformation']['PlannedCalendar']['BitmapDays']
-
-                                        newBitField = invertBitField(newBitField)   # inverse the new one, to replace the old (aka when new is 1, old is 0)
-
-                                        if start_date and end_date:
-                                            d1 = datetime.fromisoformat(start_date)
-                                            d2 = datetime.fromisoformat(end_date)
-                                            bit_changing_length = (d2-d1 + timedelta(days=1)).days   # if begins and ends on the same day, it lasts for only a day, aka result is 0, so i need to always add +1
-
-                                            d3 = datetime.fromisoformat(orig_start)
-                                            begin_index = (d1-d3).days  # neww_start - old start tells me the index when the new one begins
-                                            new_bitmap = bitDayField[:begin_index] + newBitField + bitDayField[begin_index+bit_changing_length:]
-
-                                            orig_train['CZPTTCISMessage']['CZPTTInformation']['PlannedCalendar']['BitmapDays'] = new_bitmap
-
-                                            collection_trains.replace_one({'_id':orig_train_id}, orig_train, upsert=True)
-
-                                    get_location_time(data_dict["CZPTTCISMessage"]["CZPTTInformation"]["CZPTTLocation"])
-                                    location_collection(data_dict, collection_stations)
-                                    collection_trains.replace_one({'_id':id}, data_dict, upsert=True)
-
-                                else:   # this is an original train, so act like it
-                                    get_location_time(data_dict["CZPTTCISMessage"]["CZPTTInformation"]["CZPTTLocation"])
-                                    location_collection(data_dict, collection_stations)
-                                    collection_trains.replace_one({'_id':id}, data_dict, upsert=True)
-
-                    except TypeError as te:
-                        traceback.print_exc()
-                        json_errors.append(file)
+                    root_files += 1
+                    id = getID(data_dict)  # always save core PA
+                    data_dict['_id'] = id
+                    get_location_time(data_dict["CZPTTCISMessage"]["CZPTTInformation"]["CZPTTLocation"])
+                    location_collection(data_dict, collection_stations)
+                    collection_trains.replace_one({'_id': id}, data_dict, upsert=True)
                 except ExpatError as ee:
-                    xml_errors.append(file)
+                    xml_errors.append(filename)
+
+    # process sub dirs
+    change_files = []
+    for directory in dirs:
+        print(f"parsing {directory}")
+        for filename in os.listdir(directory):
+            file_path = os.path.join(directory, filename)
+            with open(file_path, "rb") as xml_file:
+                try:
+                    if "cancel_" in xml_file.name:
+                        canceled_num += 1
+                        data_dict = xmltodict.parse(xml_file.read())
+                        canceledMessageParse(data_dict, collection_trains)
+                        # print('ok')
+                    else:  # replacement trains
+                        change_files.append(file_path)
+
+                except TypeError as te:
+                    traceback.print_exc()
+                    json_errors.append(filename)
+
+    print(f"parsing {len(change_files)} files with change")
+    for file_path in change_files:
+        if changes_num % 1000 == 0:
+            print(f"parsed {changes_num}/{len(change_files)} files")
+
+        with open(file_path, "rb") as xml_file:
+            try:
+                data_dict = xmltodict.parse(xml_file.read())
+                changes_num += 1
+                id = getID(data_dict)
+                data_dict["_id"] = id
+                orig_train_id = getIDReplaced(data_dict)  # if None, there is no original train
+                if orig_train_id:  # original trains which this one replaces was found
+                   orig_train = collection_trains.find_one(
+                       {"_id": orig_train_id})  # get the original train's info
+                   if not orig_train:  # cancelation message doesnt cancel anything
+                       pass  # if original was not found, just put this new one into the collection
+                   else:
+                       start_date = \
+                           data_dict['CZPTTCISMessage']['CZPTTInformation']['PlannedCalendar']['ValidityPeriod'][
+                               'StartDateTime']
+                       end_date = \
+                           data_dict['CZPTTCISMessage']['CZPTTInformation']['PlannedCalendar']['ValidityPeriod'][
+                               'EndDateTime']
+                       newBitField = data_dict['CZPTTCISMessage']['CZPTTInformation']['PlannedCalendar'][
+                           'BitmapDays']
+
+                       orig_start = \
+                           orig_train['CZPTTCISMessage']['CZPTTInformation']['PlannedCalendar']['ValidityPeriod'][
+                               'StartDateTime']
+                       bitDayField = orig_train['CZPTTCISMessage']['CZPTTInformation']['PlannedCalendar'][
+                           'BitmapDays']
+
+                       newBitField = invertBitField(
+                           newBitField)  # inverse the new one, to replace the old (aka when new is 1, old is 0)
+
+                       if start_date and end_date:
+                           d1 = datetime.fromisoformat(start_date)
+                           d2 = datetime.fromisoformat(end_date)
+                           bit_changing_length = (d2 - d1 + timedelta(
+                               days=1)).days  # if begins and ends on the same day, it lasts for only a day, aka result is 0, so i need to always add +1
+
+                           d3 = datetime.fromisoformat(orig_start)
+                           begin_index = (
+                                   d1 - d3).days  # neww_start - old start tells me the index when the new one begins
+                           new_bitmap = bitDayField[:begin_index] + newBitField + bitDayField[
+                                                                                  begin_index + bit_changing_length:]
+
+                           orig_train['CZPTTCISMessage']['CZPTTInformation']['PlannedCalendar'][
+                               'BitmapDays'] = new_bitmap
+
+                           collection_trains.replace_one({'_id': orig_train_id}, orig_train, upsert=True)
+
+                   get_location_time(data_dict["CZPTTCISMessage"]["CZPTTInformation"]["CZPTTLocation"])
+                   location_collection(data_dict, collection_stations)
+                   collection_trains.replace_one({'_id': id}, data_dict, upsert=True)
+
+                else:  # this is an original train, so act like it
+                   get_location_time(data_dict["CZPTTCISMessage"]["CZPTTInformation"]["CZPTTLocation"])
+                   location_collection(data_dict, collection_stations)
+                   collection_trains.replace_one({'_id': id}, data_dict, upsert=True)
+            except TypeError as te:
+                traceback.print_exc()
+                json_errors.append(filename)
+
+    print(f"Parsed \n {root_files} root files \n {canceled_num} - canceled files \n {changes_num} - changed files ")
 
     print(f"XML errors: {len(xml_errors)}")
     print(f"JSON errors: {len(json_errors)}")
-    print(f"{root_files}/{non_root_files}")
 
 def getID(xml):
     id = None
